fd71bda2e2a5c3409f3a3394f06fe651d4b734d1
James Dillard <jamesdillard@james-dillards-macbook.local>
2014-11-18 10:50:27 -0800

    fixed errors in the bls report reader test

--- tests/monthly_import_tests.py
+++ tests/monthly_import_tests.py
@@ -3,23 +3,33 @@ import os
 
 class TestReportReader(unittest.TestCase):
 	""" Test the reading of the BLS monthly Employment Situation Report """
+
+	def setUp(self):
+		""" Test setup """
+		pass
+
+	def tearDown(self):
+		""" Test teardown """
+		pass
+
+	def testReportReading(self):
 	
-	nfp09052014 = {}
+		nfp09052014 = {}
 
-	#testing the reading of the current month
-	self.assertEqual(nfp09052014.current["month"], "08")
-	self.assertEqual(nfp09052014.current["year"], "2014")
-	self.assertEqual(nfp09052014.current["jobs"], 142)
+		#testing the reading of the current month
+		self.assertEqual(nfp09052014.current["month"], "08")
+		self.assertEqual(nfp09052014.current["year"], "2014")
+		self.assertEqual(nfp09052014.current["jobs"], 142)
 
-	#testing the first revision of the previous month
-	self.assertEqual(nfp09052014.first_revision["month"], "07")
-	self.assertEqual(nfp09052014.first_revision["year"], "2014")
-	self.assertEqual(nfp09052014.first_revision["jobs"], 267)
+		#testing the first revision of the previous month
+		self.assertEqual(nfp09052014.first_revision["month"], "07")
+		self.assertEqual(nfp09052014.first_revision["year"], "2014")
+		self.assertEqual(nfp09052014.first_revision["jobs"], 267)
 
-	#testing the second revision of the previous month
-	self.assertEqual(nfp09052014.second_revision["month"], "06")
-	self.assertEqual(nfp09052014.second_revision["year"], "2014")
-	self.assertEqual(nfp09052014.second_revision["jobs"], 212)
+		#testing the second revision of the previous month
+		self.assertEqual(nfp09052014.second_revision["month"], "06")
+		self.assertEqual(nfp09052014.second_revision["year"], "2014")
+		self.assertEqual(nfp09052014.second_revision["jobs"], 212)
 
 if __name__ == "__main__":
 	unittest.main()
