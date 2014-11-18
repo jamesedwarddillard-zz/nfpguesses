f6f03fb45968c6c76ff351b9784e101045c3bf0f
James Dillard <jamesdillard@james-dillards-macbook.local>
2014-11-18 10:25:29 -0800

    added first tests and test data

---
+++ tests/monthly_import_tests.py
@@ -0,0 +1,25 @@
+import unittest
+import os
+
+class TestReportReader(unittest.TestCase):
+	""" Test the reading of the BLS monthly Employment Situation Report """
+	
+	nfp09052014 = {}
+
+	#testing the reading of the current month
+	self.assertEqual(nfp09052014.current["month"], "08")
+	self.assertEqual(nfp09052014.current["year"], "2014")
+	self.assertEqual(nfp09052014.current["jobs"], 142)
+
+	#testing the first revision of the previous month
+	self.assertEqual(nfp09052014.first_revision["month"], "07")
+	self.assertEqual(nfp09052014.first_revision["year"], "2014")
+	self.assertEqual(nfp09052014.first_revision["jobs"], 267)
+
+	#testing the second revision of the previous month
+	self.assertEqual(nfp09052014.second_revision["month"], "06")
+	self.assertEqual(nfp09052014.second_revision["year"], "2014")
+	self.assertEqual(nfp09052014.second_revision["jobs"], 212)
+
+if __name__ == "__main__":
+	unittest.main()
