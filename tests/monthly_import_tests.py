ff8c1200fc674a111008fe1312c9204d4176e92c
James Dillard <jamesdillard@james-dillards-macbook.local>
2014-11-19 12:42:58 -0800

    updated test to read a saved sample report

--- tests/monthly_import_tests.py
+++ tests/monthly_import_tests.py
@@ -3,8 +3,10 @@ import os
 import sys
 
 sys.path.append(os.path.abspath('../bls_reader'))
+sys.path.append(os.path.abspath('../past_reports'))
 
 from bls_report_classes import *
+from bls_reader import *
 
 
 class TestReportReader(unittest.TestCase):
@@ -20,28 +22,25 @@ class TestReportReader(unittest.TestCase):
 
 	def testReportReading(self):
 
-		#creating test data
-
-		june = JobsData("06", "2014", 267)
-		july = JobsData("07", "2014", 212)
-		august = JobsData("08", "2014", 142)
+		#importing a past report for a test
+		sept_2014_html = read('09052014.html')
 	
-		nfp09052014 = Report(august, july, june)
+		sept_2014_report = bls_report_reader(sept_2014_html)
 
 		#testing the reading of the current month
-		self.assertEqual(nfp09052014.current.month, "08")
-		self.assertEqual(nfp09052014.current.year, "2014")
-		self.assertEqual(nfp09052014.current.jobs, 142)
+		self.assertEqual(sept_2014_report.current.month, "08")
+		self.assertEqual(sept_2014_report.current.year, "2014")
+		self.assertEqual(sept_2014_report.current.jobs, 142)
 
 		#testing the first revision of the previous month
-		self.assertEqual(nfp09052014.first_revision.month, "07")
-		self.assertEqual(nfp09052014.first_revision.year, "2014")
-		self.assertEqual(nfp09052014.first_revision.jobs, 212)
+		self.assertEqual(sept_2014_report.first_revision.month, "07")
+		self.assertEqual(sept_2014_report.first_revision.year, "2014")
+		self.assertEqual(sept_2014_report.first_revision.jobs, 212)
 
 		#testing the second revision of the previous month
-		self.assertEqual(nfp09052014.second_revision.month, "06")
-		self.assertEqual(nfp09052014.second_revision.year, "2014")
-		self.assertEqual(nfp09052014.second_revision.jobs, 267)
+		self.assertEqual(sept_2014_report.second_revision.month, "06")
+		self.assertEqual(sept_2014_report.second_revision.year, "2014")
+		self.assertEqual(sept_2014_report.second_revision.jobs, 267)
 
 if __name__ == "__main__":
 	unittest.main()
