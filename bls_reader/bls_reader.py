54771fcd806a22ef3a005e942d79dc5021601257
James Dillard <jamesdillard@james-dillards-macbook.local>
2014-11-21 10:03:27 -0800

    updated class definitions and created nfp month finder function

--- bls_reader/bls_reader.py
+++ bls_reader/bls_reader.py
@@ -5,20 +5,30 @@ from bs4 import BeautifulSoup
 from bls_report_classes import *
 
 def nfp_table_finder(report_soup):
+	""" Identifies and returns the proper table on the Employment Situation Release """
 	report_tables = report_soup.find_all('table')
 	nfp_table = report_tables[1] #Table with NFP data is Summary Table B, the second table listed
 	return nfp_table
 
 def nfp_month_finder(nfp_table):
+	""" Finds the relevant months on the Employment Situation Release and returns them 
+	as JobsData objects """
+
+	#create the JobsData objects
+	current = JobsData()
+	first_revision = JobsData()
+	second_revision = JobsData()
+
 	nfp_table.header = nfp_table.thead.tr.find_all('th') #Turns the table header into a list of lists
-	#turn this into a dictionary
+
 	current.month = nfp_table.header[4].contents[0]
 	current.year = nfp_table.header[4].contents[2]
 	first_revision.month = nfp_table.header[3].contents[0]
 	first_revision.year = nfp_table.header[3].contents[2]
 	second_revision.month = nfp_table.header[2].contents[0]
 	second_revision.year = nfp_table.header[2].contents[0]
-	nfp_months = {}
+	
+	nfp_months = {'current': current, 'first_revision': first_revision, 'second_revision': second_revision}
 	return nfp_months
 
 def bls_report_reader(html_file):
--- bls_reader/bls_report_classes.py
+++ bls_reader/bls_report_classes.py
@@ -1,14 +1,17 @@
 """ Creating a data class for the BLS Employment Situation Report """
 
 class JobsData(object):
-	def __init__(self, month, year, jobs):
-		self.month = month #the month the data refers to (e.g., number of jobs created in June)
-		self.year = year
-		self.jobs = jobs #number of jobs created or lost in a given month in thousands
-		self.url = "" #url of the jobs report; blank by default
+	def __init__(self):
+		self.month = None #the month the data refers to (e.g., number of jobs created in June)
+		self.year = None
+		self.jobs = None #number of jobs created or lost in a given month in thousands
+		self.preliminary = True #is this month going to be revised in later months - default is True
+
 
 class Report(object):
 	def __init__(self, current_month, first_revision, second_revision):
 		self.current = current_month
 		self.first_revision = first_revision
 		self.second_revision = second_revision
+		self.release_time = None
+		self.url = "" #url of the jobs report; blank by default
